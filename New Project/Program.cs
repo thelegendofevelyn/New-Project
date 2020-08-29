3b54da27ff4c8c25ab0c2f4cf36b45d10bf93773
thelegendofevelyn <66261613+thelegendofevelyn@users.noreply.github.com>
2020-08-28 22:40:04 -0500

    Changed to guessing game

--- New Project/Program.cs
+++ New Project/Program.cs
@@ -6,7 +6,24 @@ namespace New_Project
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Welcome to the number guessing game!\nPlease guess a number between 0 and 100.");
+            Random rand = new Random();
+            int x = rand.Next(1, 10);
+            string guess = Console.ReadLine();
+            int y = Convert.ToInt32(guess);
+            bool yn = false;
+            while (yn == false)
+            {
+                Console.WriteLine("Guess Again!");
+                guess = Console.ReadLine();
+                y = Convert.ToInt32(guess);
+                if (x == y)
+                {
+                    yn = true;
+                }
+                
+            }
+            Console.WriteLine("Good job! The number was " + x);
         }
     }
 }
