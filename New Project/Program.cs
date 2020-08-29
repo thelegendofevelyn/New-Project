2c53acff44dd867ec8475dff3b7bf732dc43c30c
thelegendofevelyn <66261613+thelegendofevelyn@users.noreply.github.com>
2020-08-28 21:09:20 -0500

    hello world

--- New Project/Program.cs
+++ New Project/Program.cs
@@ -1,1 +1,12 @@
-﻿
+﻿using System;
+
+namespace New_Project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
