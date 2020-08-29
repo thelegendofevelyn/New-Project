6fb03eda540f01d89f8e0e7f966feb8629a8c024
thelegendofevelyn <66261613+thelegendofevelyn@users.noreply.github.com>
2020-08-28 20:53:22 -0500

    this is a test

--- New Project/Program.cs
+++ New Project/Program.cs
@@ -1,12 +1,1 @@
-﻿using System;
-
-namespace New_Project
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
-    }
-}
+﻿
