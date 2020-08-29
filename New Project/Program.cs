3f3d01351d6ab05f3331021de8bbc360bc48dcb7
thelegendofevelyn <66261613+thelegendofevelyn@users.noreply.github.com>
2020-08-29 11:54:03 -0500

    wrong

--- New Project/Program.cs
+++ New Project/Program.cs
@@ -11,23 +11,36 @@ namespace New_Project
             string num2 = Console.ReadLine();
             int x = Convert.ToInt32(num1);
             int y = Convert.ToInt32(num2);
+
             Console.WriteLine("\nPlease guess a number between your two numbers.");
             Random rand = new Random();
             int r = rand.Next(x, y);
             string guess = Console.ReadLine();
             int gues = Convert.ToInt32(guess);
             bool yn = false;
+
+            int counter = 0;
             while (yn == false)
             {
+                counter += 1;
                 Console.WriteLine("Guess Again!");
                 guess = Console.ReadLine();
                 gues = Convert.ToInt32(guess);
                 if (r == gues)
                 {
                     yn = true;
+                } 
+                else if(r <= gues)
+                {
+                    Console.WriteLine("Guess higher!");
+                }
+                else if(r >= gues)
+                {
+                    Console.WriteLine("Guess lower!");
                 }
 
             }
+            Console.WriteLine("Took " + counter + " tries");
             Console.WriteLine("Good job! The number was " + x);
         }
     }
